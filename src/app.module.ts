162d661e9fb5986128975be78ee2b4d4d4b4acb8
Gabriel Lira <gabrieldeio40@gmail.com>
2026-09-05 16:55:51 -0300

    feat: add account entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from './accounts/account.entity.js';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'better-sqlite3',
       database: 'db.sqlite',
+      entities: [Account],
       autoLoadEntities: true,
       synchronize: true,
     }),
